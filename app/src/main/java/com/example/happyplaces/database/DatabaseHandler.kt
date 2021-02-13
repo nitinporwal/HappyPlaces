f2a2ed5bd73b6f505231dea58471f7771a4af284
nitin-codr <nitinporwal99@gmail.com>
2021-02-14 01:30:38 +0530

    read the data from local db

--- app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
+++ app/src/main/java/com/example/happyplaces/database/DatabaseHandler.kt
@@ -2,11 +2,15 @@ package com.example.happyplaces.database
 
 import android.content.ContentValues
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteException
 import android.database.sqlite.SQLiteOpenHelper
 import com.example.happyplaces.models.HappyPlaceModel
+import java.lang.Exception
 
-class DatabaseHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
+class DatabaseHandler(context: Context) :
+    SQLiteOpenHelper(context, DATABASE_NAME, null, DATABASE_VERSION) {
     companion object {
         private const val DATABASE_VERSION = 1
         private const val DATABASE_NAME = "HappyPlacesDatabase"
@@ -23,7 +27,8 @@ class DatabaseHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME
     }
 
     override fun onCreate(db: SQLiteDatabase?) {
-        val CREATE_HAPPY_PLACE_TABLE = ("CREATE TABLE $TABLE_HAPPY_PLACES ($KEY_ID INTEGER PRIMARY KEY, $KEY_TITLE TEXT, $KEY_IMAGE TEXT, $KEY_DESCRIPTION TEXT, $KEY_DATE TEXT, $KEY_LOCATION TEXT, $KEY_LATITUDE TEXT, $KEY_LONGIUTDE TEXT)")
+        val CREATE_HAPPY_PLACE_TABLE =
+            ("CREATE TABLE $TABLE_HAPPY_PLACES ($KEY_ID INTEGER PRIMARY KEY, $KEY_TITLE TEXT, $KEY_IMAGE TEXT, $KEY_DESCRIPTION TEXT, $KEY_DATE TEXT, $KEY_LOCATION TEXT, $KEY_LATITUDE TEXT, $KEY_LONGIUTDE TEXT)")
         db?.execSQL(CREATE_HAPPY_PLACE_TABLE)
     }
 
@@ -47,5 +52,36 @@ class DatabaseHandler(context: Context): SQLiteOpenHelper(context, DATABASE_NAME
         val result = db.insert(TABLE_HAPPY_PLACES, null, contentValue)
         db.close()
         return result
+    }
+
+    fun getHappyPlacesList(): ArrayList<HappyPlaceModel> {
+        val happyPlaceList: ArrayList<HappyPlaceModel> = ArrayList<HappyPlaceModel>()
+
+        val selectQuery = "SELECT * FROM $TABLE_HAPPY_PLACES"
+        val db = this.readableDatabase
+        try {
+            val cursor: Cursor = db.rawQuery(selectQuery, null)
+            if (cursor.moveToFirst()) {
+                do {
+                    val place = HappyPlaceModel(
+                        cursor.getInt(cursor.getColumnIndex(KEY_ID)),
+                        cursor.getString(cursor.getColumnIndex(KEY_TITLE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_IMAGE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_DESCRIPTION)),
+                        cursor.getString(cursor.getColumnIndex(KEY_DATE)),
+                        cursor.getString(cursor.getColumnIndex(KEY_LOCATION)),
+                        cursor.getDouble(cursor.getColumnIndex(KEY_LATITUDE)),
+                        cursor.getDouble(cursor.getColumnIndex(KEY_LONGIUTDE))
+                    )
+                    happyPlaceList.add(place)
+                } while (cursor.moveToNext())
+            }
+            cursor.close()
+        } catch (e: SQLiteException) {
+            db.execSQL(selectQuery)
+            return ArrayList()
+        }
+
+        return happyPlaceList
     }
 }
