97ece28d25bff4efe48ac0972faaee40d4b26b14
nitin-codr <nitinporwal99@gmail.com>
2021-02-14 02:20:19 +0530

    prepare the recyclerview and use third party library for nice round images

--- app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
+++ app/src/main/java/com/example/happyplaces/activities/MainActivity.kt
@@ -4,7 +4,10 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.happyplaces.R
+import com.example.happyplaces.adapters.HappyPlacesAdapter
 import com.example.happyplaces.database.DatabaseHandler
 import com.example.happyplaces.models.HappyPlaceModel
 import kotlinx.android.synthetic.main.activity_main.*
@@ -14,22 +17,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        fabAddHappyPlace.setOnClickListener{
+        fabAddHappyPlace.setOnClickListener {
             val intent = Intent(this, AddHappyPlaceActivity::class.java)
             startActivity(intent)
         }
         getHappyPlacesListFromLocalDB()
     }
 
+    private fun setupHappyPlacesRecylerView(happyPlaceList: ArrayList<HappyPlaceModel>) {
+        rv_happy_places_list.layoutManager = LinearLayoutManager(this)
+        rv_happy_places_list.setHasFixedSize(true)
+        val placesAdapter = HappyPlacesAdapter(this, happyPlaceList)
+        rv_happy_places_list.adapter = placesAdapter
+    }
+
     private fun getHappyPlacesListFromLocalDB() {
         val dbHandler = DatabaseHandler(this)
         val getHappyPlaceList: ArrayList<HappyPlaceModel> = dbHandler.getHappyPlacesList()
 
-        if(getHappyPlaceList.size>0) {
-            for(i in getHappyPlaceList) {
-                Log.e("Title", i.title)
-                Log.e("Description", i.description)
-            }
+        if (getHappyPlaceList.size > 0) {
+            rv_happy_places_list.visibility = View.VISIBLE
+            tv_no_records_available.visibility = View.GONE
+            setupHappyPlacesRecylerView(getHappyPlaceList)
+        } else {
+            rv_happy_places_list.visibility = View.GONE
+            tv_no_records_available.visibility = View.VISIBLE
         }
     }
 }
