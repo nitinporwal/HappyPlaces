06d6bd4c19daec0283022c0a20f4c058e1e3e1f8
nitin-codr <nitinporwal99@gmail.com>
2021-02-14 13:32:21 +0530

    add an onclicklistener to recyclerview element

--- app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
+++ app/src/main/java/com/example/happyplaces/adapters/HappyPlacesAdapter.kt
@@ -12,6 +12,7 @@ import kotlinx.android.synthetic.main.activity_add_happy_place.view.iv_place_ima
 import kotlinx.android.synthetic.main.item_happy_place.view.*
 
 open class HappyPlacesAdapter(private val context: Context, private val list: ArrayList<HappyPlaceModel>): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private var onClickListener: OnClickListener? = null
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
         return MyViewHolder(
             LayoutInflater.from(context).inflate(
@@ -29,11 +30,25 @@ open class HappyPlacesAdapter(private val context: Context, private val list: Ar
             holder.itemView.iv_place_image.setImageURI(Uri.parse(model.image))
             holder.itemView.tvTitle.text = model.title
             holder.itemView.tvDescription.text = model.description
+
+            holder.itemView.setOnClickListener{
+                if(onClickListener != null) {
+                    onClickListener!!.onClick(position, model)
+                }
+            }
         }
     }
 
     override fun getItemCount(): Int {
         return list.size
+    }
+
+    interface OnClickListener {
+        fun onClick(position: Int, model: HappyPlaceModel)
+    }
+
+    fun setOnClickListener(onClickListener: OnClickListener) {
+        this.onClickListener = onClickListener
     }
 
     private class MyViewHolder(view: View): RecyclerView.ViewHolder(view)
